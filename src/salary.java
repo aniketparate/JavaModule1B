6f69653c9a7372fda746970873b0252772f98d3f
aniketparate <aniketvparate@gmail.com>
2021-09-16 14:36:24 +0530

    add

--- src/salary.java
+++ src/salary.java
@@ -11,15 +11,14 @@ PT= Rs100/-
 */
 public class salary {
     public static void main(String[] args) {
-        float basic = 25000;
-        float DA, HRA, CCA, PF, PT;
-        float gross, net;
+        float basic = 25000, DA, HRA, CCA = 240, PF, PT, gross, net;
 
         DA = (float) (0.7 * basic);
         HRA = (float) (0.3 * basic);
-        CCA = 240;
-        gross =  DA + HRA + CCA + basic;
 
-        System.out.println(" Gross =    "+gross);
+        gross =  DA + HRA + CCA + basic;
+        net = gross - (float) (0.1 * basic) - 100;
+
+        System.out.println(" Gross =   and Net =  "+gross+"   "+net);
     }
 }
