02a7640853e736ebc77d34c083b865119d1a46b9
aniketparate <aniketvparate@gmail.com>
2021-09-16 15:42:48 +0530

    add

--- src/ATM.java
+++ src/ATM.java
@@ -1,3 +1,21 @@
-public class ATM {
+/*
+Pooja would like to withdraw X $US from an ATM.
+The cash machine will only accept the transaction if X is a multiple of 5,
+and Pooja's account balance has enough cash to perform the
+withdrawal transaction (including bank charges).
+For each successful withdrawal the bank charges 0.50 $US.
+Calculate Pooja's account balance after an attempted transaction.
+*/
 
+public class ATM {
+    public static void main(String[] args) {
+        float withdrawal = 0,balance=3000, X=1500;
+
+        if (withdrawal>0 && withdrawal<2000 && X%5 == 0) {
+            withdrawal = X + 0.15F;
+            System.out.println(" balance =  " +(balance - withdrawal));
+        }
+        else
+            System.out.println(" Withdrawal Unsuccessful");
+    }
 }
