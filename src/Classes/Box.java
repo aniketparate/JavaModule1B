6e835ad62267c3dff4340f8e47b1840a5df43bf0
aniketparate <aniketvparate@gmail.com>
2021-09-20 15:11:28 +0530

    add

---
+++ src/Classes/Box.java
@@ -0,0 +1,13 @@
+package Classes;
+
+class Box {
+    double weight;
+    double depth;
+    double height;
+    double width;
+
+    double volume(){
+        return width * height * depth;
+    }
+}
+
--- src/Classes/UseBox.java
+++ src/Classes/UseBox.java
@@ -1,9 +1,6 @@
-class Box {
-    double weight;
-    double depth;
-    double height;
-    double width;
-}
+package Classes;
+
+import Classes.Box;
 
 public class UseBox{
     public static void main(String args[]) {
@@ -26,12 +23,10 @@ public class UseBox{
         b.weight = 8;
         b.width = 8;
 
-        volume1 = a.height * a.width * a.depth;
-        volume2 = b.height * b.width * b.depth;
+        volume1 = a.volume();
+        volume2 = b.volume();
 
         System.out.println("Volume " + volume1);
         System.out.println("Volume " + volume2);
-
     }
-
 }
