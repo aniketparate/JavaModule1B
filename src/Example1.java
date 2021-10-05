65b029f407c0c1e05109cc4c23b36a6cc0e64b5c
aniketparate <aniketvparate@gmail.com>
2021-10-05 10:16:42 +0530

    addup codes

--- src/Example1.java
+++ src/Example1.java
@@ -1,2 +1,19 @@
+class Student{
+    int rollno;//
+
+
+    void display(){
+
+    }
+}
+
 public class Example1 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+
+        s1.display();
+        s2.display();
+    }
 }
